18477858632052490039e5250580530070cc46d1
Liam Blumenstein-Tait <liam.bt@gmail.com>
2026-01-09 21:54:32 +0100

    5.7

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,34 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+  const [buttonText, setButtonText] = useState('view')
+  
+  const handleClick = () => {
+    setButtonText(buttonText === 'view' ? 'hide' : 'view')
+    setVisible(buttonText === 'view' ? true : false)
+  }
+  
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+     {blog.title} {blog.author}
+      <button onClick={handleClick}>{buttonText}</button>
+      {visible && (
+        <div>
+         <div><a>{blog.url}</a></div>
+         <div>likes {blog.likes} <button>like</button></div>
+        </div>
+      )}
+    </div>  
+  )
+}
 
 export default Blog
