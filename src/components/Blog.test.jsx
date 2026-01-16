c1d7e1ee79cc1bfd3c9ef6590d1d90537e7da185
Liam Blumenstein-Tait <liam.bt@gmail.com>
2026-01-16 19:43:54 +0100

    5.15

--- src/components/Blog.test.jsx
+++ src/components/Blog.test.jsx
@@ -14,11 +14,10 @@ const blog = {
 }
 
 const testUser = {
-    name: 'supertester'
+  name: 'supertester'
 }
 
 describe('Blog Component', () => {
-
   test('displays only blog title and author', () => {
     render(<Blog blog={blog} user={testUser}/>)
 
@@ -30,6 +29,7 @@ describe('Blog Component', () => {
     expect(blogLikes).toBeNull()
     expect(blogUser).toBeNull()
   }),
+
   test('blog url and likes are shown after show-button is pressed', async () => {
     render(<Blog blog={blog} user={testUser}/>)
 
@@ -42,5 +42,21 @@ describe('Blog Component', () => {
 
     expect(blogLikes).toBeDefined()
     expect(blogUser).toBeDefined()
+  }),
+
+  test('like button event handler is called the correct number of times', async () => {
+    const mockLikeHandler = vi.fn()
+    
+    render(<Blog blog={blog} user={testUser} updateLikes={mockLikeHandler}/>)
+    
+    const user = userEvent.setup()
+    const viewButton = screen.getByText('view')
+    await user.click(viewButton)
+    
+    const likeButton = screen.getByText('like')
+    await user.click(likeButton)
+    await user.click(likeButton)
+
+    expect(mockLikeHandler.mock.calls).toHaveLength(2)
   })
 })
